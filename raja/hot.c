e2f98bc48482e26d5743394ab5b741b81fceb24d
Matt Martineau <m.martineau@hotmail.co.uk>
2018-07-13 12:19:57 +0000

    Optimised the CG performance for RAJA.

--- raja/hot.c
+++ raja/hot.c
@@ -68,18 +68,21 @@ double initialise_cg(const int nx, const int ny, const int pad, const double dt,
 // https://inldigitallibrary.inl.gov/sti/3952796.pdf
 // Take the average of the coefficients at the cells surrounding
 // each face
-  RAJA::forall<exec_policy>(RAJA::RangeSegment(pad, ny-pad), [=] RAJA_DEVICE (int ii) {
-    for (int jj = pad; jj < (nx + 1) - pad; ++jj) {
-      s_x[(ii) * (nx + 1) + (jj)] =
-          (dt * conductivity *
-           (density[(ii)*nx + (jj)] + density[(ii)*nx + (jj - 1)])) /
-          (2.0 * density[(ii)*nx + (jj)] * density[(ii)*nx + (jj - 1)] *
-           edgedx[jj] * edgedx[jj] * heat_capacity);
-    }
+  RAJA::forall<exec_policy>(RAJA::RangeSegment(0, (nx+1)*ny), [=] RAJA_DEVICE (int i) {
+      const int ii = i / (nx+1);
+      const int jj = i % (nx+1);
+      if(ii >= pad && ii < ny-pad && jj >= pad && jj < (nx+1)-pad) {
+        s_x[(ii) * (nx + 1) + (jj)] =
+        (dt * conductivity * (density[(ii)*nx + (jj)] + density[(ii)*nx + (jj - 1)])) /
+        (2.0 * density[(ii)*nx + (jj)] * density[(ii)*nx + (jj - 1)] *
+         edgedx[jj] * edgedx[jj] * heat_capacity);
+      }
   });
 
-  RAJA::forall<exec_policy>(RAJA::RangeSegment(pad, (ny+1)-pad), [=] RAJA_DEVICE (int ii) {
-    for (int jj = pad; jj < nx - pad; ++jj) {
+  RAJA::forall<exec_policy>(RAJA::RangeSegment(0, nx*(ny+1)), [=] RAJA_DEVICE (int i) {
+      const int ii = i / nx;
+      const int jj = i % nx;
+      if(ii >= pad && ii < (ny+1)-pad && jj >= pad && jj < nx-pad) {
       s_y[(ii)*nx + (jj)] =
           (dt * conductivity *
            (density[(ii)*nx + (jj)] + density[(ii - 1) * nx + (jj)])) /
@@ -89,8 +92,10 @@ double initialise_cg(const int nx, const int ny, const int pad, const double dt,
   });
 
   RAJA::ReduceSum<reduce_policy, double> initial_r2(0.0);
-  RAJA::forall<exec_policy>(RAJA::RangeSegment(pad, ny-pad), [=] RAJA_DEVICE (int ii) {
-    for (int jj = pad; jj < nx - pad; ++jj) {
+  RAJA::forall<exec_policy>(RAJA::RangeSegment(0, nx*ny), [=] RAJA_DEVICE (int i) {
+      const int ii = i / nx;
+      const int jj = i % nx;
+      if(ii >= pad && ii < ny-pad && jj >= pad && jj < nx-pad) {
       r[(ii)*nx + (jj)] =
           temperature[(ii)*nx + (jj)] -
           ((s_y[(ii)*nx + (jj)] + s_x[(ii) * (nx + 1) + (jj)] + 1.0 +
@@ -118,8 +123,10 @@ double calculate_pAp(const int nx, const int ny, const int pad,
   // You don't need to use a matrix as the band matrix is fully predictable
   // from the 5pt stencil
   RAJA::ReduceSum<reduce_policy, double> pAp(0.0);
-  RAJA::forall<exec_policy>(RAJA::RangeSegment(pad, ny-pad), [=] RAJA_DEVICE (int ii) {
-    for (int jj = pad; jj < nx - pad; ++jj) {
+  RAJA::forall<exec_policy>(RAJA::RangeSegment(0, nx*ny), [=] RAJA_DEVICE (int i) {
+      const int ii = i / nx;
+      const int jj = i % nx;
+      if(ii >= pad && ii < ny-pad && jj >= pad && jj < nx-pad) {
       Ap[(ii)*nx + (jj)] =
           (s_y[(ii)*nx + (jj)] + s_x[(ii) * (nx + 1) + (jj)] + 1.0 +
            s_x[(ii) * (nx + 1) + (jj + 1)] + s_y[(ii + 1) * nx + (jj)]) *
@@ -142,8 +149,10 @@ double calculate_new_r2(const int nx, const int ny, const int pad, double alpha,
   START_PROFILING(&compute_profile);
 
   RAJA::ReduceSum<reduce_policy, double> new_r2(0.0);
-  RAJA::forall<exec_policy>(RAJA::RangeSegment(pad, ny-pad), [=] RAJA_DEVICE (int ii) {
-    for (int jj = pad; jj < nx - pad; ++jj) {
+  RAJA::forall<exec_policy>(RAJA::RangeSegment(0, nx*ny), [=] RAJA_DEVICE (int i) {
+      const int ii = i / nx;
+      const int jj = i % nx;
+      if(ii >= pad && ii < ny-pad && jj >= pad && jj < nx-pad) {
       temperature[(ii)*nx + (jj)] += alpha * p[(ii)*nx + (jj)];
       r[(ii)*nx + (jj)] -= alpha * Ap[(ii)*nx + (jj)];
       new_r2 += r[(ii)*nx + (jj)] * r[(ii)*nx + (jj)];
@@ -158,8 +167,10 @@ double calculate_new_r2(const int nx, const int ny, const int pad, double alpha,
 void update_conjugate(const int nx, const int ny, const int pad,
                       const double beta, const double* r, double* p) {
   START_PROFILING(&compute_profile);
-  RAJA::forall<exec_policy>(RAJA::RangeSegment(pad, ny-pad), [=] RAJA_DEVICE (int ii) {
-    for (int jj = pad; jj < nx - pad; ++jj) {
+  RAJA::forall<exec_policy>(RAJA::RangeSegment(0, nx*ny), [=] RAJA_DEVICE (int i) {
+      const int ii = i / nx;
+      const int jj = i % nx;
+      if(ii >= pad && ii < ny-pad && jj >= pad && jj < nx-pad) {
       p[(ii)*nx + (jj)] = r[(ii)*nx + (jj)] + beta * p[(ii)*nx + (jj)];
     }
   });
